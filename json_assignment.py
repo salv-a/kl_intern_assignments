295e045f1aedd17295b8c5b3d08c25dbf2a8072a
salv-a <130457967+salv-a@users.noreply.github.com>
2023-05-17 09:46:35 +0530

    Add files via upload

---
+++ json_assignment.py
@@ -0,0 +1,17 @@
+import json
+with open(r"C:\Users\salvamol.km\Downloads\sample_data.json", "r") as file:
+    content = json.load(file)
+
+outer_dict={}
+parameter_content=content["parametersList"]
+for one in parameter_content:
+    inner_dict={}
+    key=one["parameterName"]
+    inner_dict["min"] = one["min"]
+    inner_dict["max"] = one["max"]
+    inner_dict["avg"] = one["avg"]
+    outer_dict[key]=inner_dict
+
+print(outer_dict)
+with open(r"C:\Users\salvamol.km\Downloads\sample_data_out.json", "w") as file:
+    json.dump(outer_dict,file)
